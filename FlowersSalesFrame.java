28901ebf2a9f2014e0d926aae89b046b115b9e37
CostaHu <hyc2723@gmail.com>
2016-05-25 23:53:39 +0800

    Add the top area for the main panel

--- FlowersSalesFrame.java
+++ FlowersSalesFrame.java
@@ -1,5 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by CostaHu on 2016/5/11.
  */
-public class FlowersSalesFrame {
+public class FlowersSalesFrame extends JFrame{
+
+    private JButton jbt_kc = new JButton("显示库存");
+    private JButton jbt_tb = new JButton("销售图表");
+    private  JButton jbt_se = new JButton("保存并退出");
+
+    //Init the constructor
+    public FlowersSalesFrame(){
+        JPanel panel = new JPanel();
+        panel.add(jbt_kc);
+        panel.add(jbt_tb);
+        panel.add(jbt_se);
+        this.add(panel,BorderLayout.NORTH);
+    }
 }
--- TestProg.java
+++ TestProg.java
@@ -1,5 +1,15 @@
+import javax.swing.*;
+
 /**
  * Created by CostaHu on 2016/5/11.
  */
 public class TestProg {
+    public static void main(String[] args) {
+        JFrame frame = new FlowersSalesFrame();
+        frame.setSize(500,400);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("销售统计");
+        frame.setVisible(true);
+    }
 }
